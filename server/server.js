962045f1bbe10a33e3091061102691daf277f0cf
ErlingMMM <71938112+ErlingMMM@users.noreply.github.com>
2022-03-01 20:28:57 +0100

    test

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 
 
 const app = express();
+const test = 0;
 
 app.get("/login", (req, res, next) => {
     res.json( {username: "Noen Andre"} )
@@ -16,3 +17,4 @@ app.post("/login", (req, res, next) => {
 const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Started on http://localhost:${server.address().port}`)
 });
+
