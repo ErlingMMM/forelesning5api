faafb1c96a7f0443030c55a3cb6495e27c61babc
ErlingMMM <71938112+ErlingMMM@users.noreply.github.com>
2022-03-05 20:03:04 +0100

    syntax typo fix

--- server/server.js
+++ server/server.js
@@ -3,14 +3,13 @@ import express from "express";
 const app = express();
 
 app.get("/login", (req, res, next) => {
-  res.json({ username: "Noen Andre" });
+  res.json({ username: "Noen André Persson" });
 });
-
 app.post("/login", (req, res, next) => {
   res.sendStatus(401);
 });
 
-app.use(express.static("..client/dist"));
+app.use(express.static("../client/dist"));
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
