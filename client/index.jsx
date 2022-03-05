b47c4c85137097096b34920aba13157dfaafa3f9
ErlingMMM <71938112+ErlingMMM@users.noreply.github.com>
2022-03-05 21:04:07 +0100

    api fix

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter,
+  HashRouter,
+  Link,
+  Route,
+  Routes,
+} from "react-router-dom";
 
-ReactDOM.render(<h1>Hello Express1</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <ul>
+        <li>
+          <Link to={"/login"}>Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <form>
+      <h1>Login</h1>
+      <div>
+        Username <input />
+      </div>
+      <div>
+        Password <input type={"password"} />
+      </div>
+      <div>
+        <button>Login</button>
+      </div>
+    </form>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/*"} element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
